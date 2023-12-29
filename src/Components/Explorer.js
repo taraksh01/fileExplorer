a2b80f08973cba43d28ab2980f772782bf1b7968
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-29 11:43:20 +0530

    Added buttons for edit, delete and new folder or file creation

--- src/Components/Explorer.js
+++ src/Components/Explorer.js
@@ -6,12 +6,29 @@ const Explorer = ({ data }) => {
     <>
       {data?.isFolder ? (
         <>
-          <div
-            className="p-1 text-xl font-medium hover:bg-gray-300"
-            onClick={() => setHideFolder(!hideFolder)}
-          >
-            <span className="p-1">{!hideFolder ? "🔽" : "▶️"}</span>📁{" "}
-            {data.name}
+          <div className="p-1 hover:bg-gray-300 flex items-center justify-between">
+            <span
+              className="p-1 text-yellow-500 text-xl font-medium"
+              onClick={() => setHideFolder(!hideFolder)}
+            >
+              {!hideFolder ? "🔽" : "▶️"}📁 {data.name}
+            </span>
+            <span className="flex gap-1 text-lg">
+              <button className="relative">
+                📁
+                <span className="absolute -bottom-1.5 right-0.5 text-blue-600">
+                  +
+                </span>
+              </button>
+              <button className="relative">
+                📄
+                <span className="absolute -bottom-1.5 right-0.5 text-blue-600">
+                  +
+                </span>
+              </button>
+              <button>✏️</button>
+              <button>❌</button>
+            </span>
           </div>
           <div className={`${hideFolder ? "hidden" : "block pl-5"}`}>
             {data.children.map((child) => (

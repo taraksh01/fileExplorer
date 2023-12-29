133128717765597ec22041674eafa75d39513944
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-29 10:59:46 +0530

    Added folder opening and closing feature

--- src/Components/FolderStructure.js
+++ src/Components/FolderStructure.js
@@ -1,17 +1,26 @@
+import { useState } from "react";
+
 const FolderStructure = ({ data }) => {
+  const [hideFolder, setHideFolder] = useState(true);
   return (
     <>
       {data?.isFolder ? (
         <>
-          <div className="p-1 text-xl font-medium">📁 {data.name}</div>
-          <div className="ml-5">
+          <div
+            className="p-1 text-xl font-medium hover:bg-gray-300"
+            onClick={() => setHideFolder(!hideFolder)}
+          >
+            <span className="p-1">{!hideFolder ? "🔽" : "▶️"}</span>📁{" "}
+            {data.name}
+          </div>
+          <div className={`${hideFolder ? "hidden" : "block pl-5"}`}>
             {data.children.map((child) => (
               <FolderStructure data={child} key={child.id} />
             ))}
           </div>
         </>
       ) : (
-        <div className="p-1">📄 {data.name}</div>
+        <div className="p-1 hover:bg-gray-300">📄 {data.name}</div>
       )}
     </>
   );

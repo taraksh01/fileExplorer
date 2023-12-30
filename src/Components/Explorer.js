7b74006facc3a811a60c0679c08b4b284d1cb607
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-12-30 10:47:45 +0530

    Created function to handle new file or folder addition

--- src/Components/Explorer.js
+++ src/Components/Explorer.js
@@ -2,6 +2,32 @@ import { useState } from "react";
 
 const Explorer = ({ data }) => {
   const [hideFolder, setHideFolder] = useState(true);
+  const [showInput, setShowInput] = useState({
+    isVisible: false,
+    isFolder: true,
+  });
+
+  const handleNewFile = (e, isFolder = true) => {
+    e.stopPropagation();
+    setHideFolder(false);
+    setShowInput({
+      isVisible: true,
+      isFolder,
+    });
+  };
+
+  const addNewFile = (e) => {
+    e.preventDefault();
+    const newFile = {
+      id: Date.now(),
+      name: e.target[0].value,
+      isFolder: showInput.isFolder,
+      children: [],
+    };
+    data.children.push(newFile);
+    setShowInput({ isVisible: false });
+  };
+
   return (
     <>
       {data?.isFolder ? (
@@ -14,13 +40,16 @@ const Explorer = ({ data }) => {
               {!hideFolder ? "🔽" : "▶️"}📁 {data.name}
             </span>
             <span className="flex gap-1 text-lg">
-              <button className="relative">
+              <button className="relative" onClick={(e) => handleNewFile(e)}>
                 📁
                 <span className="absolute -bottom-1.5 right-0.5 text-blue-600">
                   +
                 </span>
               </button>
-              <button className="relative">
+              <button
+                className="relative"
+                onClick={(e) => handleNewFile(e, false)}
+              >
                 📄
                 <span className="absolute -bottom-1.5 right-0.5 text-blue-600">
                   +
@@ -31,6 +60,22 @@ const Explorer = ({ data }) => {
             </span>
           </div>
           <div className={`${hideFolder ? "hidden" : "block pl-5"}`}>
+            {showInput.isVisible && (
+              <div className="flex gap-2 items-center text-lg m-1">
+                <span className="text-xl">
+                  {showInput.isFolder ? "📁" : "📄"}
+                </span>
+                <form onSubmit={(e) => addNewFile(e)}>
+                  <input
+                    className="border border-black outline-none px-1 rounded-md"
+                    type="text"
+                    autoFocus
+                    placeholder={showInput.isFolder ? "New Folder" : "New File"}
+                    onBlur={() => setShowInput({ isVisible: false })}
+                  />
+                </form>
+              </div>
+            )}
             {data.children.map((child) => (
               <Explorer data={child} key={child.id} />
             ))}
